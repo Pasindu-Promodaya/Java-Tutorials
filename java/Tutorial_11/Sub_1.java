c768f688c6f7ce3e62fa830fa293c0b2f1bff2e1
Pasindu Promodaya Weragala <pasinduweragala20@gmail.com>
2025-06-10 00:10:10 +0530

    Delete java/Tutorial_11 directory

--- java/Tutorial_11/Sub_1.java
+++
@@ -1,4 +0,0 @@
-package Tutorial_11;
-
-public class Sub_1 extends Super_1 { //Can't inherit data from Super Class because it use "final" keyword
-}
--- java/Tutorial_11/Super_1.java
+++
@@ -1,4 +0,0 @@
-package Tutorial_11;
-
-public final class Super_1 {
-}
